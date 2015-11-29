657f0dc38861558425430b2ee409b9f45bd0e1f5
RajaReddy <rajareddy511@gmail.com>
2015-11-29 23:08:54 +0530

    Initial commit

---
+++ app/src/main/java/com/raja/hubbleconnected/utils/Global.java
@@ -0,0 +1,19 @@
+package com.raja.hubbleconnected.utils;
+
+import android.util.Log;
+
+/**
+ * Created by Rajareddy on 28/11/15.
+ */
+public class Global {
+
+    private final static boolean show = true;
+    private final static String TAG = "Rss";
+
+    public static String RSS_URL = "RSS_URL";
+
+    public static void showLog(String msg) {
+        if(show)
+            Log.i(TAG, msg);
+    }
+}
